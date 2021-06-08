9a58074c1492fc942dfd5da6d9119395b7df851b
Zach Coyle <zach.coyle@gmail.com>
2021-06-08 00:05:05 -0400

    devshell

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "naersk flake template";
+  description = "CLI search and replace | Space Age seD";
 
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
+    devshell.url = github:numtide/devshell;
     naersk.url = "github:nmattia/naersk";
     fenix = {
       url = "github:nix-community/fenix";
@@ -12,12 +13,15 @@
     sad-src = { url = github:ms-jpq/sad; flake = false; };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, naersk, sad-src }:
+  outputs = { self, nixpkgs, flake-utils, devshell, fenix, naersk, sad-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ fenix.overlay ];
+          overlays = [
+            devshell.overlay
+            fenix.overlay
+          ];
         };
         naersk-lib = naersk.lib.${system}.override {
           inherit (fenix.packages.${system}.minimal) cargo rustc;
@@ -37,8 +41,8 @@
 
         defaultApp = apps.sad;
 
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
+        devShell = pkgs.devshell.mkShell {
+          packages = with pkgs; [
             fenix.packages.${system}.minimal.cargo
             fenix.packages.${system}.minimal.rustc
           ];
